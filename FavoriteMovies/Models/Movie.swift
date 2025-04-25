0de8f92044389a86d49d2f1beab33fa24f60556c
DimMelDev <dimdevpost@gmail.com>
2025-04-25 19:14:49 +0300

    added Movie model

---
+++ FavoriteMovies/Models/Movie.swift
@@ -0,0 +1,16 @@
+//
+//  Movie.swift
+//  FavoriteMovies
+//
+//  Created by Dmytro Melnyk on 25.04.2025.
+//
+
+import Foundation
+
+struct Movie {
+    let id: Int
+    let title: String
+    let overview: String
+    let releaseDate: String
+    let posterURL: String
+}
