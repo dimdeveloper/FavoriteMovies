5993c59bf5940f460631a7ea07311a73b8471269
DimMelDev <dimdevpost@gmail.com>
2025-04-28 00:08:09 +0300

    changing root view

--- FavoriteMovies/SceneDelegate.swift
+++ FavoriteMovies/SceneDelegate.swift
@@ -18,7 +18,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = MovieListViewController()
+        let movieListVC = MovieListViewController()
+        let navigationController = UINavigationController(rootViewController: movieListVC)
+        window.rootViewController = navigationController
         window.makeKeyAndVisible()
 
         self.window = window
